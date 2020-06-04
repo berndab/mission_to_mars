bee5483bb268b168ed1c8e5cda03d1297d8cc316
Dean Bernocchi <dean.bernocchi@gmail.com>
2020-06-03 20:26:15 -0500

    Added comments

--- scraping.py
+++ scraping.py
@@ -10,6 +10,7 @@ import datetime as dt
 
 
 def scrape_all():
+
     # Initiate browser with headless driver for deployment
     browser = Browser("chrome", executable_path="chromedriver", headless=True)
 
@@ -33,23 +34,23 @@ def scrape_all():
 
 def mars_news(browser):
 
-    # Visit the mars nasa news site
+    # Mars' NASA newsite webpage URL
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
 
     # Optional delay for loading the page
     browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
-    # Convert the browser html to a soup object and then quit the browser
+    # Convert the browser html to a soup object
     html = browser.html
     news_soup = BeautifulSoup(html, 'html.parser')
 
-    slide_elem = news_soup.select_one('ul.item_list li.slide')
-    slide_elem.find("div", class_='content_title')
-
     try:
 
-        # Use the parent element to find the first `a` tag and save it as `news_title`
+        # Find first element in the html list
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+        # Use the parent element to find the div containing the new title
         news_title = slide_elem.find("div", class_='content_title').get_text()
 
         # Use the parent element to find the paragraph text
@@ -63,22 +64,22 @@ def mars_news(browser):
 
 def featured_image(browser):
 
-    # Visit URL
+    # Mars' featured image webpage URL
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url)
 
-    # Find and click the full image button
+    # Find the full image button and click
     full_image_elem = browser.find_by_id('full_image')
     full_image_elem.click()
 
 
-    # Find the more info button and click that
+    # Find the more info button and click
     browser.is_element_present_by_text('more info', wait_time=1)
     more_info_elem = browser.links.find_by_partial_text('more info')
     more_info_elem.click()
 
 
-    # Parse the resulting html with soup
+    # Parse the resulting html
     html = browser.html
     img_soup = BeautifulSoup(html, 'html.parser')
 
@@ -99,37 +100,55 @@ def featured_image(browser):
 def mars_facts():
     
     try:
-        # use 'read_html" to scrape the facts table into a dataframe
+        # Read the Mars data html table into a DataFrame
+        # using the Mars' fact webpage URL
         df = pd.read_html('http://space-facts.com/mars/')[0]
     except BaseException:
         return None
 
+    # Add DataFrame column names
     df.columns=['Description', 'Value']
 
+    # Convert the Mars fact DataFrame to a list
+    # of dictionaries with one fact per row
     return df.to_dict("records")
 
 def mars_hemispheres(browser):
 
-    # Mars’ hemispheres webpage
+    # Mars' hemispheres webpage URL
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
 
+    # List of titles of the images to retrieve
     image_title_list = ["Cerberus Hemisphere", "Schiaparelli Hemisphere", "Syrtis Major Hemisphere", "Valles Marineris Hemisphere"]
 
+    # List to store image data
     image_data_list = []
 
     for title in image_title_list:
+
+        # Start on the hemisphere homepage
         browser.visit(url)
 
+        # Find link to full size hemisphere image
         element = browser.links.find_by_partial_text(title)
         element.click()
 
         html = browser.html
-        parser = BeautifulSoup(html, 'html.parser')
-        img_url = parser.select_one("div.downloads ul li a").get("href")
+        img_soup = BeautifulSoup(html, 'html.parser')
 
+        try:
+            
+            # Get hemisphere full size image URL
+            img_url = img_soup.select_one("div.downloads ul li a").get("href")
+
+        except AttributeError:
+            return None
+
+        # Append the hemisphere image data dictionary to the list
         image_data_list.append({"title" : title, "img_url" : img_url})
 
     return image_data_list
+
 
 if __name__ == "__main__":
     # If running as script, print scraped data
